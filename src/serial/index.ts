0144732cb91d90c62fe69f4f1c31956f7be9d12c
Mqxx <62719703+Mqxx@users.noreply.github.com>
2023-08-02 16:58:18 +0200

    Added JSDoc to functions of class

--- src/serial/index.ts
+++ src/serial/index.ts
@@ -8,49 +8,78 @@ export default class Serial {
      * 
      * @public
      * @static
-     * @example
-     * ```
-     * [
-     *   {
-     *     path: '/dev/ttyACM0',
-     *     manufacturer: 'Arduino (www.arduino.cc)',
-     *     serialNumber: '752303138333518011C1',
-     *     pnpId: 'usb-Arduino__www.arduino.cc__0043_752303138333518011C1-if00',
-     *     locationId: undefined,
-     *     productId: '0043',
-     *     vendorId: '2341'
-     *   },
-     *   ...
-     * ]
-     * ```
      */
     public static getPortsInfo() {
 
     }
 
     /**
-     * Opens the connection to the serial device.
+     * Opens the connection to the serial device. The connection must be closed.
+     * Throws a `ConnectionAlreadyOpenedError` if the connection is already opened.
      * 
      * @public
      */
-    public open() {
+    public open() : void {
 
     }
 
-    public close() {
+    /**
+     * Closes the connection to the serial device. The connection must be opened.
+     * Throws a `ConnectionAlreadyClosedError` if the connection is already closed.
+     * 
+     * @public
+     */
+    public close() : void {
 
     }
 
-    public read() {
-
+    /**
+     * Reads up to `buffer.byteLength` bytes into `buffer` from the serial input buffer.
+     * It resolves to the number of bytes read (`0` < `n` <= `buffer.byteLength`) and
+     * rejects if any error encountered or the timeout is expired.
+     * 
+     * @public
+     * @param buffer Buffer to read characters into
+     * @param options Read options
+     * @returns Resolves/Rejects to the number of bytes read
+     */
+    public read(
+        buffer : Uint8Array,
+        options : {}
+    ) : Promise<number> {
+        return new Promise(() => 0);
     }
 
-    public readUntil() {
-
+    /**
+     * Reads up to `buffer.byteLength` bytes into `buffer` from the serial input buffer
+     * until a specific search pattern is found.
+     * It resolves to the number of bytes read (`0` < `n` <= `buffer.byteLength`) and
+     * rejects if any error encountered or the timeout is expired.
+     * 
+     * @public
+     * @param buffer Buffer to read characters into
+     * @param pattern Search pattern
+     * @param options Read options
+     * @returns Resolves/Rejects to the number of bytes read
+     */
+    public readUntil(
+        buffer : Uint8Array,
+        pattern : string,
+        options : {}
+    ) : Promise<number> {
+        return new Promise(() => 0);
     }
 
-    public write() {
-
+    /**
+     * Writes up to `buffer.byteLength` bytes to `buffer` to the serial output buffer.
+     * 
+     * @public
+     */
+    public write(
+        buffer : Uint8Array,
+        bytes : number
+    ) : Promise<number> {
+        return new Promise(() => 0);
     }
 
     public clearBufferIn() {
@@ -65,4 +94,33 @@ export default class Serial {
         return;
     }
 
+    public get sizeBufferIn() {
+        return;
+    }
+
+    public get sizeBufferOut() {
+        return;
+    }
+
+    public get port() {
+        return;
+    }
+
+    public get baudrate() {
+        return;
+    }
+    
+    public get parity() {
+        return;
+    }
+    
+    public get dataBits() {
+        return;
+    }
+
+    public get stopBits() {
+        return;
+    }
 }
+
+Deno.stdin.read()
