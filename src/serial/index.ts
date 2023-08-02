8c58bf21ca254a778992fcb6fa9f1eb8462b5912
Mqxx <62719703+Mqxx@users.noreply.github.com>
2023-08-02 12:48:49 +0200

    feat: :sparkles: Added class functions

--- src/serial/index.ts
+++ src/serial/index.ts
@@ -1,3 +1,68 @@
 export default class Serial {
-    
+    constructor() {
+
+    }
+
+    /**
+     * Returns a list of port objects.
+     * 
+     * @public
+     * @static
+     * @example
+     * ```
+     * [
+     *   {
+     *     path: '/dev/ttyACM0',
+     *     manufacturer: 'Arduino (www.arduino.cc)',
+     *     serialNumber: '752303138333518011C1',
+     *     pnpId: 'usb-Arduino__www.arduino.cc__0043_752303138333518011C1-if00',
+     *     locationId: undefined,
+     *     productId: '0043',
+     *     vendorId: '2341'
+     *   },
+     *   ...
+     * ]
+     * ```
+     */
+    public static getPortsInfo() {
+
+    }
+
+    /**
+     * Opens the connection to the serial device.
+     * 
+     * @public
+     */
+    public open() {
+
+    }
+
+    public close() {
+
+    }
+
+    public read() {
+
+    }
+
+    public readUntil() {
+
+    }
+
+    public write() {
+
+    }
+
+    public clearBufferIn() {
+
+    }
+
+    public clearBufferOut() {
+
+    }
+
+    public get isOpen() {
+        return;
+    }
+
 }
