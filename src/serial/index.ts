a773025525575c3f23bb5f0bc964edcdf8e907b3
Mqxx <62719703+Mqxx@users.noreply.github.com>
2023-08-03 09:24:28 +0200

    Updated/Added JSDoc, Updated option type

--- src/serial/index.ts
+++ src/serial/index.ts
@@ -1,3 +1,6 @@
+import { ReadOptions } from "./read_options.d.ts";
+import { WriteOptions } from "./write_options.d.ts";
+
 export default class Serial {
     constructor() {
 
@@ -39,13 +42,13 @@ export default class Serial {
      * rejects if any error encountered or the timeout is expired.
      * 
      * @public
-     * @param buffer Buffer to read characters into
+     * @param buffer Buffer to read bytes into
      * @param options Read options
      * @returns Resolves/Rejects to the number of bytes read
      */
     public read(
         buffer : Uint8Array,
-        options : {}
+        options : ReadOptions
     ) : Promise<number> {
         return new Promise(() => 0);
     }
@@ -57,7 +60,7 @@ export default class Serial {
      * rejects if any error encountered or the timeout is expired.
      * 
      * @public
-     * @param buffer Buffer to read characters into
+     * @param buffer Buffer to read bytes into
      * @param pattern Search pattern
      * @param options Read options
      * @returns Resolves/Rejects to the number of bytes read
@@ -65,61 +68,111 @@ export default class Serial {
     public readUntil(
         buffer : Uint8Array,
         pattern : string,
-        options : {}
+        options : ReadOptions
     ) : Promise<number> {
         return new Promise(() => 0);
     }
 
     /**
-     * Writes up to `buffer.byteLength` bytes to `buffer` to the serial output buffer.
+     * Writes up to `buffer.byteLength` bytes from `buffer` into the serial output buffer.
+     * It resolves to the number of bytes written (`0` < `n` <= `buffer.byteLength`) and
+     * rejects if any error encountered or the timeout is expired.
      * 
      * @public
+     * @param buffer Buffer to write bytes from
+     * @param options Write options
+     * @returns Resolves/Rejects to the number of bytes written
      */
     public write(
         buffer : Uint8Array,
-        bytes : number
+        options : WriteOptions
     ) : Promise<number> {
         return new Promise(() => 0);
     }
 
-    public clearBufferIn() {
-
+    /**
+     * Clears the local input buffer.
+     * It resolves as soon as the input buffer is successfully cleared and rejects if any error encountered.
+     * 
+     * @public
+     * @returns Resolves after successful clear/Rejects if any error encountered
+     */
+    public clearBufferIn() : Promise<void> {
+        return new Promise(() => {});
     }
 
-    public clearBufferOut() {
-
+    /**
+     * Clears the local output buffer.
+     * It resolves as soon as the output buffer is successfully cleared and rejects if any error encountered.
+     * 
+     * @public
+     * @returns Resolves after successful clear/Rejects if any error encountered
+     */
+    public clearBufferOut() : Promise<void> {
+        return new Promise(() => {});
     }
 
-    public get isOpen() {
-        return;
+    /**
+     * @public
+     * @type isOpen
+     */
+    public get isOpen() : boolean {
+        return false;
     }
 
-    public get sizeBufferIn() {
-        return;
-    }
-
-    public get sizeBufferOut() {
-        return;
-    }
-
-    public get port() {
-        return;
-    }
-
-    public get baudrate() {
-        return;
+    /**
+     * @public
+     * @type sizeBufferIn
+     */
+    public get sizeBufferIn() : number {
+        return 0;
     }
     
-    public get parity() {
-        return;
-    }
-    
-    public get dataBits() {
-        return;
+    /**
+     * @public
+     * @type sizeBufferOut
+     */
+    public get sizeBufferOut() : number {
+        return 0;
     }
 
-    public get stopBits() {
-        return;
+    /**
+     * @public
+     * @type port
+     */
+    public get port() : string {
+        return '';
+    }
+
+    /**
+     * @public
+     * @type baudrate
+     */
+    public get baudrate() : number {
+        return 0;
+    }
+    
+    /**
+     * @public
+     * @type parity
+     */
+    public get parity() : number {
+        return 0;
+    }
+    
+    /**
+     * @public
+     * @type dataBits
+     */
+    public get dataBits() : number {
+        return 0;
+    }
+
+    /**
+     * @public
+     * @type stopBits
+     */
+    public get stopBits() : number {
+        return 0;
     }
 }
-
