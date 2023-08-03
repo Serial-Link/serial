2a60da8b9c56c4de7c3c056fde3a03d6a920c9d6
Mqxx <62719703+Mqxx@users.noreply.github.com>
2023-08-03 10:19:10 +0200

    Added JSDoc

--- src/serial/interface/port.d.ts
+++ src/serial/interface/port.d.ts
@@ -1,5 +1,25 @@
+/**
+ * This interface represents a port object.
+ * 
+ * @interface
+ */
 export interface Port {
+    /**
+     * Full name of the port (E.g. `ttyACM0` or `COM5`)
+     * 
+     * @property
+     */
     name : string,
+
+    /**
+     * Prefix of the port name (E.g. `ttyACM` or `COM`)
+     * 
+     * @property
+     */
     prefix : string,
+
+    /**
+     * Index of the port name (E.g. `0` or `5`)
+     */
     index : number
 }
