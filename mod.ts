6797ab1a3db2598ff8cc5990f04ccca6446a4ae5
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-01-08 17:19:33 +0100

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,13 +1,13 @@
-import Serial from "./src/serial/index.ts";
-import { baudrates } from "./src/serial/constant/baudrates.ts";
-import { dataBits } from "./src/serial/constant/data_bits.ts";
-import { parities } from "./src/serial/constant/parities.ts";
-import { stopBits } from "./src/serial/constant/stop_bits.ts";
+import Serial from "./src/serial.ts";
+import { baudrates } from "./src/constant/baudrates.ts";
+import { dataBits } from "./src/constant/data_bits.ts";
+import { parities } from "./src/constant/parities.ts";
+import { stopBits } from "./src/constant/stop_bits.ts";
 
-import { Baudrate } from "./src/serial/type/baudrate.d.ts";
-import { DataBits } from "./src/serial/type/data_bits.d.ts";
-import { Parity } from "./src/serial/type/parity.d.ts";
-import { StopBits } from "./src/serial/type/stop_bits.d.ts";
+import { Baudrate } from "./src/type/baudrate.ts";
+import { DataBits } from "./src/type/data_bits.ts";
+import { Parity } from "./src/type/parity.ts";
+import { StopBits } from "./src/type/stop_bits.ts";
 
 export {
     Serial,
